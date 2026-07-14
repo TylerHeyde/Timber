fa5409204b5c72119c6d2e7736bf6c3126bd1c55
Tyler <tylerheyde1@gmail.com>
2026-07-13 17:17:27 -0700

    Added Background rendering

--- code/Timber.cpp
+++ code/Timber.cpp
@@ -1,17 +1,36 @@
 #include <SFML/Graphics.hpp>
 
+
 using namespace sf;
 int main()
 {
 	VideoMode vm(1920, 1080);
-	RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
+	RenderWindow window(vm, "Timber!!!", Style::Default);
 	
-	while (window.isOpen()) {
+	Texture textureBackground;
+	textureBackground.loadFromFile("graphics/background.png");
+	Sprite spriteBackground;
+	spriteBackground.setTexture(textureBackground);
+	spriteBackground.setPosition(0, 0);
+
+
+	// ======================== game loop ===========================
+	while (window.isOpen()){
+		Event event;
+		while (window.pollEvent(event)){
+			if (event.type == Event::Closed) {
+				window.close();
+			}
+		}
 		if (Keyboard::isKeyPressed(Keyboard::Escape)) {
 			window.close();
 		}
-		window.clear();
+		if (Keyboard::isKeyPressed(Keyboard::F11)) {
+			
+		}
 
+		window.clear();
+		window.draw(spriteBackground);
 		window.display();
 	}
 
